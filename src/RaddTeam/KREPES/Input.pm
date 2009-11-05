f738e4ac0f8ad7ab0135ff5ae57e0ff8760aa97b
Katrina Payne <fullmetalharlot@nimhlabs.com>
2009-11-05 11:52:39 -0700

    this is more of a test commit more than anything else

---
+++ src/RaddTeam/KREPES/Input.pm
@@ -0,0 +1,22 @@
+#
+#===============================================================================
+#
+#         FILE:  Input.pm
+#
+#  DESCRIPTION:  Input abstraction.
+#
+#        FILES:  ---
+#         BUGS:  ---
+#        NOTES:  Just setting this file up for future use
+#       AUTHOR:  KatrinaTheLamia (FullMetalHarlot@NIMHLabs.Com), 
+#      COMPANY:  NIMH Labs -- Radd Team.
+#      VERSION:  0.1
+#      CREATED:  11/05/2009 11:51:13 AM
+#     REVISION:  ---
+#===============================================================================
+
+use strict;
+use warnings;
+
+
+
